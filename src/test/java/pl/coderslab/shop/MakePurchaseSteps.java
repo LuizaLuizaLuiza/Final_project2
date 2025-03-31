deb1ac5d59b3e3765ea2db7f783916a4958131dc
Luiza <luiza.marczak7@gmail.com>
2025-03-31 12:01:57 +0200

    Zadanie2

--- src/test/java/pl/coderslab/shop/MakePurchaseSteps.java
+++ src/test/java/pl/coderslab/shop/MakePurchaseSteps.java
@@ -4,9 +4,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
